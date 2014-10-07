22ebedbeef0bb2b2207c5adb674dd517c55e0caa
Kirill Basin <kir.basin@gmail.com>
2014-10-08 10:55:56 +1100

    Making multiple children

--- Assets/Fractal.cs
+++ Assets/Fractal.cs
@@ -1,6 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Fractal : MonoBehaviour {
+public class Fractal : MonoBehaviour
+{
+    public Mesh mesh;
+    public Material material;
+    public int maxDepth;
+    public float childScale;
+    private int depth = 0;
+
+    private void Initialize(Fractal parent, Vector3 direction)
+    {
+        mesh = parent.mesh;
+        material = parent.material;
+        maxDepth = parent.maxDepth;
+        depth = parent.depth + 1;
+        childScale = parent.childScale;
+        transform.parent = parent.transform;
+        transform.localScale = Vector3.one * childScale;
+        transform.localPosition = direction * (0.5f + 0.5f * childScale);
+    }
+    
+    private void Start()
+    {
+        gameObject.AddComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>().material = material;
+        if (depth < maxDepth)
+        {
+            new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.up);
+            new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.right);
+        }
+    }
 
 }
